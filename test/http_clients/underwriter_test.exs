2e80c93ff1a627f5a511a8f2765972c26205e6da
Gabriel Carraro <gacarraro@gmail.com>
2021-01-29 15:28:32 -0300

    removes unecessary asserts

--- test/http_clients/underwriter_test.exs
+++ test/http_clients/underwriter_test.exs
@@ -11,29 +11,25 @@ defmodule HttpClients.UnderwriterTest do
 
   describe "client/3" do
     test "returns a tesla client" do
-      expected_client = %Tesla.Client{
-        adapter: nil,
-        fun: nil,
-        post: [],
-        pre: [
-          {Tesla.Middleware.BaseUrl, :call, [@base_url]},
-          {Tesla.Middleware.JSON, :call, [[]]},
-          {Tesla.Middleware.Headers, :call,
+      expected_configs = [
+        {Tesla.Middleware.BaseUrl, :call, [@base_url]},
+        {Tesla.Middleware.JSON, :call, [[]]},
+        {Tesla.Middleware.Headers, :call,
+         [
            [
-             [
-               {"X-Ambassador-Client-ID", @client_id},
-               {"X-Ambassador-Client-Secret", @client_secret}
-             ]
-           ]},
-          {Tesla.Middleware.Retry, :call, [[delay: 1000, max_retries: 3]]},
-          {Tesla.Middleware.Timeout, :call, [[timeout: 15_000]]},
-          {Tesla.Middleware.Logger, :call, [[]]},
-          {Goodies.Tesla.Middleware.RequestIdForwarder, :call, [[]]}
-        ]
-      }
+             {"X-Ambassador-Client-ID", @client_id},
+             {"X-Ambassador-Client-Secret", @client_secret}
+           ]
+         ]},
+        {Tesla.Middleware.Retry, :call, [[delay: 1000, max_retries: 3]]},
+        {Tesla.Middleware.Timeout, :call, [[timeout: 15_000]]},
+        {Tesla.Middleware.Logger, :call, [[]]},
+        {Goodies.Tesla.Middleware.RequestIdForwarder, :call, [[]]}
+      ]
 
       client = Underwriter.client(@base_url, @client_id, @client_secret)
-      assert client == expected_client
+      assert %Tesla.Client{} = client
+      assert client.pre == expected_configs
     end
   end
 
