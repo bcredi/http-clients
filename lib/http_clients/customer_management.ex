70642941f7b6f519efb94381f2f8bbc7810d0180
Mateus <youalreadydid@users.noreply.github.com>
2021-04-08 13:37:37 -0300

    [SPRF-1014] add "GET /v1/proponents/:id" route for CustomerManagement (#13)

--- lib/http_clients/customer_management.ex
+++ lib/http_clients/customer_management.ex
@@ -12,6 +12,15 @@ defmodule HttpClients.CustomerManagement do
     end
   end
 
+  @spec get_proponent(Tesla.Client.t(), String.t()) :: {:error, any} | {:ok, Tesla.Env.t()}
+  def get_proponent(%Tesla.Client{} = client, proponent_id) do
+    case Tesla.get(client, "/v1/proponents/#{proponent_id}") do
+      {:ok, %Tesla.Env{status: 200} = response} -> {:ok, response}
+      {:ok, %Tesla.Env{} = response} -> {:error, response}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
   @spec client(String.t(), String.t(), String.t()) :: Tesla.Client.t()
   def client(base_url, client_id, client_secret) do
     headers = authorization_headers(client_id, client_secret)
