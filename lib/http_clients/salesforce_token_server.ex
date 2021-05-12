57f14b6ab99149d23689453919af325b50da33e7
Mateus <youalreadydid@users.noreply.github.com>
2021-05-12 15:03:40 -0300

    [SPRF-1097] add SalesforceTokenServer (#18)

---
+++ lib/http_clients/salesforce_token_server.ex
@@ -0,0 +1,93 @@
+defmodule HttpClients.SalesforceTokenServer do
+  @moduledoc """
+  An agent that stores a valid Salesforce API token.
+
+  First setup it on your `application.ex`, e.g.:
+
+  ```elixir
+  config = [
+    url: "https://login.salesforce.com",
+    client_id: "some client_id",
+    client_secret: "some client_secret",
+    username: "fulano@creditas.com",
+    password: "somepassword123",
+    grant_type: "password"
+  ]
+  children = [{HttpClients.SalesforceTokenServer, name: MyApp.SalesforceTokenServer, config: config}]
+  Supervisor.start_link(children, name: MyApp.Supervisor)
+  ```
+
+  Then you can retrieve the token:
+
+  ```elixir
+  HttpClients.SalesforceTokenServer.get_token(MyApp.SalesforceTokenServer)
+  ```
+
+  Also you can update the token when needed:
+
+  ```elixir
+  HttpClients.SalesforceTokenServer.update_token(MyApp.SalesforceTokenServer)
+  ```
+
+  Or you can get a new token and set it by yourself:
+
+  ```elixir
+  config = [
+    url: "https://login.salesforce.com",
+    client_id: "some client_id",
+    client_secret: "some client_secret",
+    username: "fulano@creditas.com",
+    password: "somepassword123",
+    grant_type: "password"
+  ]
+  {:ok, token} = HttpClients.SalesforceTokenServer.request_new_token(config)
+  HttpClients.SalesforceTokenServer.set_token(MyApp.SalesforceTokenServer, token)
+  ```
+  """
+  use Agent
+  require Logger
+
+  @spec start_link(keyword()) :: {:ok, pid()} | {:error, any()} | no_return()
+  def start_link(opts) when is_list(opts) do
+    {config, opts} = Keyword.pop!(opts, :config)
+
+    Agent.start_link(
+      fn ->
+        {:ok, token} = request_new_token(config)
+        Logger.info("#{__MODULE__} started with pid #{inspect(self())}")
+        %{token: token, config: config}
+      end,
+      opts
+    )
+  end
+
+  @doc "Request an authenticated token to Salesforce"
+  @spec request_new_token(keyword()) ::
+          {:ok, ExForce.OAuthResponse.t()} | {:error, any()} | no_return()
+  def request_new_token(config) when is_list(config) do
+    {url, payload} = Keyword.pop!(config, :url)
+    ExForce.OAuth.get_token(url, payload)
+  end
+
+  @doc "Gets a token from the given TokenServer"
+  @spec get_token(pid()) :: ExForce.OAuthResponse.t()
+  def get_token(server) when is_pid(server) do
+    Agent.get(server, & &1[:token])
+  end
+
+  @doc "Request an authenticated token to Salesforce and set it to the given TokenServer"
+  @spec update_token(pid()) :: :ok | no_return()
+  def update_token(server) when is_pid(server) do
+    Logger.info("Updating the salesforce token")
+    config = Agent.get(server, & &1[:config])
+    {:ok, token} = request_new_token(config)
+    set_token(server, token)
+  end
+
+  @doc "Set a new token for the given TokenServer"
+  @spec set_token(pid(), ExForce.OAuthResponse.t()) :: :ok
+  def set_token(server, %ExForce.OAuthResponse{} = new_token) when is_pid(server) do
+    Logger.info("Setting new token for #{inspect(server)}")
+    Agent.update(server, &Map.put(&1, :token, new_token))
+  end
+end
---
+++ test/http_clients/salesforce_token_server_test.exs
@@ -0,0 +1,116 @@
+defmodule HttpClients.SalesforceTokenServerTest do
+  use ExUnit.Case
+  import Tesla.Mock
+
+  alias HttpClients.SalesforceTokenServer
+
+  @salesforce_url "https://login.salesforce.com"
+
+  @config [
+    url: @salesforce_url,
+    client_id: "some client_id",
+    client_secret: "some client_secret",
+    username: "fulano@creditas.com",
+    password: "somepassword123",
+    grant_type: "password"
+  ]
+
+  @token_response %{
+    "access_token" => "access_token_foo",
+    "instance_url" => "https://example.com",
+    "id" => "https://example.com/id/fakeid",
+    "token_type" => "Bearer",
+    "issued_at" => "1505149885697",
+    "signature" => "+HM1VVxVzTAkwHLmcEiRrFoQDEiZm8H0QfALenayXg0="
+  }
+
+  @token %ExForce.OAuthResponse{
+    access_token: @token_response["access_token"],
+    id: @token_response["id"],
+    instance_url: @token_response["instance_url"],
+    signature: @token_response["signature"],
+    token_type: @token_response["token_type"],
+    refresh_token: nil,
+    scope: nil,
+    issued_at:
+      @token_response["issued_at"] |> String.to_integer() |> DateTime.from_unix!(:millisecond)
+  }
+
+  setup do
+    mock_global(fn %{method: :post, url: "#{@salesforce_url}/services/oauth2/token"} ->
+      json(@token_response)
+    end)
+
+    {:ok, pid: start_supervised!({SalesforceTokenServer, config: @config})}
+  end
+
+  describe "start_link/1" do
+    setup do
+      stop_supervised(SalesforceTokenServer)
+      :ok
+    end
+
+    test "raises error when there's no config" do
+      assert_raise RuntimeError, ~r/key :config not found in: \[]/, fn ->
+        start_supervised!({SalesforceTokenServer, []})
+      end
+    end
+
+    test "raises error when there's no url on config" do
+      assert_raise RuntimeError, ~r/key :url not found in: \[]/, fn ->
+        start_supervised!({SalesforceTokenServer, config: []})
+      end
+    end
+
+    test "starts the server with a valid token" do
+      pid = start_supervised!({SalesforceTokenServer, config: @config})
+
+      Agent.get(pid, fn state ->
+        assert state[:token] == @token
+        assert state[:config] == @config
+      end)
+    end
+  end
+
+  describe "get_token/1" do
+    test "returns a token", %{pid: pid} do
+      assert SalesforceTokenServer.get_token(pid) == @token
+    end
+  end
+
+  describe "update_token/1" do
+    test "updates a token", %{pid: pid} do
+      token_response = Map.put(@token_response, "token_type", "some token_type")
+      expected_token = Map.put(@token, :token_type, "some token_type")
+
+      mock_global(fn %{method: :post, url: "#{@salesforce_url}/services/oauth2/token"} ->
+        json(token_response)
+      end)
+
+      :ok = SalesforceTokenServer.update_token(pid)
+      assert SalesforceTokenServer.get_token(pid) == expected_token
+    end
+  end
+
+  describe "request_new_token/1" do
+    test "raises error without url" do
+      {_, config} = Keyword.pop!(@config, :url)
+
+      assert_raise KeyError, ~r/key :url not found in/, fn ->
+        SalesforceTokenServer.request_new_token(config)
+      end
+    end
+
+    test "requests a new token" do
+      assert SalesforceTokenServer.request_new_token(@config) == {:ok, @token}
+    end
+  end
+
+  describe "set_token/2" do
+    test "stores a token", %{pid: pid} do
+      token = %ExForce.OAuthResponse{}
+      :ok = SalesforceTokenServer.set_token(pid, token)
+      assert SalesforceTokenServer.get_token(pid) == token
+    end
+  end
+end
