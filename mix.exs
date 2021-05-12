75b189e587bfd6f78b0e0c81a83fbbc7d9e33065
Mateus Rambo <mateus.rstrey@gmail.com>
2021-05-12 13:26:44 -0300

    add :ex_force as optional dependency

--- mix.exs
+++ mix.exs
@@ -31,6 +31,7 @@ defmodule HttpClients.MixProject do
       {:tesla, "~> 1.4.0"},
       {:jason, ">= 1.0.0"},
       {:goodies, github: "bcredi/goodies"},
+      {:ex_force, "~> 0.3", optional: true},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.13", only: :test},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
